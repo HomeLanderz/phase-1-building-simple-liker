78bd6c47b2d6ede6a0722ebc530fbe859170f7eb
Sohail Ahmed <ahnrd43@gmail.com>
2022-11-06 12:31:10 +0300

    Done with proud

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const likeButtons = document.querySelectorAll("span.like-glyph")
+likeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    mimicServerCall()
+      .then(resp => {
+        if (button.textContent === '♥') {
+          button.classList.remove("activated-heart")
+          button.textContent = '♡'
+          console.log("Unliked")
+        } else {
+          button.classList.add("activated-heart")
+          button.textContent = '♥'
+          console.log("Done")
+        }
+      })
+      .catch(error => {
+        const errorDiv = document.getElementById("modal")
+        errorDiv.classList.remove("hidden")
+        console.log("Error")
+        setTimeout(function(){
+          errorDiv.classList.add("hidden")
+        }, 3000)
+      })
+    })
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
